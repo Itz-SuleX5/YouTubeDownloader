3e4a91797429399ffabc05df430e44bd09dcebe4
Itz-SuleX5 <AA23028@ues.edu.sv>
2024-11-23 10:06:36 -0800

    Update Django settings for production

--- youtube_downloader/youtube_downloader/settings.py
+++ youtube_downloader/youtube_downloader/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s##d(!a95no*ytp4e42*f#7i*kz4=qf#xi*@h2k^8k_2&iwunj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['youtubedownloader-pkcx.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -120,10 +120,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# CORS settings
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    'https://youtubedownloader-frontend.onrender.com',
+    'http://localhost:3000',
+]
+CORS_ALLOW_CREDENTIALS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CORS_ALLOW_ALL_ORIGINS = True
